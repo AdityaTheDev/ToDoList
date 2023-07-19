cc6c11f3bcad609cb0f542825b846134acef6ed3
Aditya <84168117+AdityaTheDev@users.noreply.github.com>
2023-07-19 20:32:00 +0530

    Update2 app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const casechange=require(__dirname+'/namecasechange.js')
 const mongoose=require('mongoose')
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://aditya26:aditya26@cluster0.byjngqy.mongodb.net/todolistDB",{useNewUrlParser:true});
 
 const itemSchema={
     name:String
